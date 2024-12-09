e2f85d797f9df24691966beb4ef5b0676cfee459
Vadim Popov <vadik@VPopov-Zephyrus>
2024-12-09 09:53:12 +0100

    Local build changes

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=rof84cI37x5kcPy3mg7b
+export POSTGRES_HOST=database-vp-homework.ctzxbiv7qa8o.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139719802809
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+export NODE_OPTIONS=--openssl-legacy-provider
